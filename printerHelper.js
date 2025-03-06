b5740560f9735743d5b9173d8bbe8a2eaf6c932a
baldisserarodrigo <rohbaldissera86@gmail.com>
2025-03-06 06:26:43 -0300

    menuPrincipal
    ajustes no menu principal

--- printerHelper.js
+++ printerHelper.js
@@ -2,73 +2,83 @@
 const prompt = require("prompt-sync")()
 console.clear()
 
-import { gasConta } from "./gasosa"
-
 //MENU PRINCIPAL
-function menuPrincipal(a) {
+let menuPrincipal = ['Sair','Manutenção de Hardware','Ajustes/Modo Manutenção','Código de Erro', 'Firmwares',
+    'Calculadora de Vida Útil','Calculadora de Combustível','Check-lists','Toners/Cartuchos','Equipamentos'
+]
+function menuPrincipal() {
     console.log('')
     console.log('=================================')
     console.log('|  P R I N T E R   H E L P E R  |')
     console.log('=================================')
-    console.log('|[ 1] Manutenção de Hardware    |')
-    console.log('|[ 2] Configurações e Ajustes   |')
-    console.log('|[ 3] Modos de Manutenção       |')
-    console.log('|[ 4] Códigos de Erro           |')
-    console.log('|[ 5] Firmwares                 |')
-    console.log('|[ 6] Calculadora de Vida Útil  |')
-    console.log('|[ 7] Calculadora de Combustível|')
-    console.log('|[ 8] Check-lists               |')
-    console.log('|[ 9] Toners/Cartuchos          |')
-    console.log('|[10] Equipamentos              |')
-    console.log('|[11] Sair                      |')
+    console.log('|[1] Manutenção de Hardware     |')
+    console.log('|[2] Ajustes/Modo Manutenção    |')
+    console.log('|[3] Códigos de Erro            |')
+    console.log('|[4] Firmwares                  |')
+    console.log('|[5] Calculadora de Vida Útil   |')
+    console.log('|[6] Calculadora de Combustível |')
+    console.log('|[7] Check-lists                |')
+    console.log('|[8] Toners/Cartuchos           |')
+    console.log('|[9] Equipamentos               |')
+    console.log('|[0] Sair                       |')
     console.log('=================================')
     console.log('')
-    a = parseInt(prompt('Digite a opção desejada: '))
-    console.clear()
-    return a
+}
+//MANUTENÇÃO DE HARDWARE
+function manutencaoHardware() {
+    console.log('')
+    console.log('=================================')
+    console.log('|    MANUTENÇÃO DE HARDWARE     |')
+    console.log('=================================')
+    console.log('|[1] KYOCERA                    |')
+    console.log('|[2] RICOH                      |')
+    console.log('|[3] LEXMARK                    |')
+    console.log('|[4] CANON                      |')
+    console.log('|[5] Voltar                     |')
+    console.log('|[0] Sair                       |')
+    console.log('=================================')
+    console.log('')
 }
 
-let opcao = 0
 
-while (opcao<1 || opcao>11) {
-    opcao = menuPrincipal(opcao)
-    if (opcao<1 || opcao>11) {
-        console.log('OPÇÃO ERRADA')
-    }
-}
 
+
+
+let opcao = null
+menuPrincipal()
+opcao = parseInt(prompt('Digite a opção desejada: '))
 switch(opcao) {
     case 1:
-        console.log('Manutenção de Hardware')
+        manutencaoHardware()
+        opcao = parseInt(prompt('Escolha o fabricante, voltar ou sair: '))
+
         break
     case 2:
-        console.log('Configurações e Ajustes')
+        console.log('Ajustes/Modo Manutenção')
         break
     case 3:
-        console.log('Modos de Manutenção')
-        break
-    case 4:
         console.log('Códigos de Erro')
         break
-    case 5:
+    case 4:
         console.log('Firmwares')
         break
-    case 6:
+    case 5:
         console.log('Calculadora de Vida Útil')
         break
-    case 7:
-        gasConta()
+    case 6:
+        console.log('Calculadora de Combustível')
         break
-    case 8:
+    case 7:
         console.log('Check-lists')
         break
-    case 9:
+    case 8:
         console.log('Toners/Cartuchos')
         break
-    case 10:
+    case 9:
         console.log('Equipamentos')
         break
-    case 11:
+    case 0:
         console.log('Saindo...')
+        break
 }
 
