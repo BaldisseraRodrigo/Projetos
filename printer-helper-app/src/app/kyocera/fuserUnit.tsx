b0cf09f46322954471f1651b65137c8b74c1f3e7
baldisserarodrigo <rohbaldissera86@gmail.com>
2025-10-02 21:59:05 -0300

    update

--- printer-helper-app/src/app/kyocera/fuserUnit.tsx
+++ printer-helper-app/src/app/kyocera/fuserUnit.tsx
@@ -6,60 +6,70 @@ export default function FuserUnit() {
 
     return (
         <View>
+            <ScrollView style={styles.container2}>
             <View>
                 <Header subtitle='Troca da Unidade Fusora'/>
             </View>
-            <ScrollView style={styles.container2}>
+
                 <View style={styles.row}>
                     <Text style={styles.data}>I. DESLIGUE o equipamento e RETIRE o cabo de força.</Text>
                     <View style={styles.container3}>
-                        <Image style={styles.image} source={require("../../../assets/images/m2040.png")} />
+                        <Image style={styles.image} source={require("../../../assets/images/001_m2040.png")} />
                     </View>
 
                     <Text style={styles.data}>II. ABRA a tampa traseira e com uma chave de fenda fina, RETIRE os acabamentos laterais.</Text>
-                                        <View style={styles.container3}>
-                        <Image style={styles.image} source={require("../../../assets/images/m2040.png")} />
+                    <View style={styles.container3}>
+                        <Image style={styles.image} source={require("../../../assets/images/002_traseira.png")} />
                     </View>
                     <Text style={styles.data}>III. Com a chave de fenda, PRESSIONE a drobradiça direita da tampa traseira para soltar.</Text>
-                                        <View style={styles.container3}>
-                        <Image style={styles.image} source={require("../../../assets/images/m2040.png")} />
+                    <View style={styles.container3}>
+                        <Image style={styles.image} source={require("../../../assets/images/003_dobra.png")} />
                     </View>
                     <Text style={styles.data}>IV. RETIRE os outros acabamentos.</Text>
-                                        <View style={styles.container3}>
-                        <Image style={styles.image} source={require("../../../assets/images/m2040.png")} />
+                    <View style={styles.container3}>
+                        <Image style={styles.image} source={require("../../../assets/images/004_tampa_tras.png")} />
+                        <Image style={styles.image} source={require("../../../assets/images/005_acabamento.png")} />
                     </View>
                     <Text style={styles.data}>V. DESCONECTE os cabos de sinal e força da unidade fusora.</Text>
-                                        <View style={styles.container3}>
-                        <Image style={styles.image} source={require("../../../assets/images/m2040.png")} />
+                    <View style={styles.container3}>
+                        <Image style={styles.image} source={require("../../../assets/images/006_cabos.png")} />
                     </View>
                     <Text style={styles.data}>VI. SOLTE os quatro parafusos com uma chave philips e REMOVA a unidade fusora</Text>
-                                        <View style={styles.container3}>
-                        <Image style={styles.image} source={require("../../../assets/images/m2040.png")} />
+                    <View style={styles.container3}>
+                        <Image style={styles.image7} source={require("../../../assets/images/007_parafusos.png")} />
                     </View>
                     <Text style={styles.data}>VII. REMOVA a unidade fusora.</Text>
-                                        <View style={styles.container3}>
-                        <Image style={styles.image} source={require("../../../assets/images/m2040.png")} />
+                    <View style={styles.container3}>
+                        <Image style={styles.image} source={require("../../../assets/images/008_remover.png")} />
                     </View>
+                    <Text style={styles.data}>VIII. MONTE a nova unidade fusora, e APERTE ao menos 2 parafusos para testar antes de montar os acabamentos.</Text>
+                    <View style={styles.container3}>
+                        <Image style={styles.image} source={require("../../../assets/images/009_montar.png")} />
+                    </View>
+                    <Text style={styles.data}>IX. CONECTE os cabos da unidade de fusão no equipamento, LIGUE o equipamento e IMPRIMA uma página de teste.</Text>
+                    <View style={styles.container3}>
+                        <Image style={styles.image} source={require("../../../assets/images/010_conectar.png")} />
+                    </View>
+                    <Text style={styles.data}>X. Para finalizar, MONTE as placas de acabamento e a tampa traseira no equipamento novamente.</Text>
+                </View>
+                <View style={styles.container}>
+                    <BackButton/>
                 </View>
             </ScrollView>
-
-            <View style={styles.container}>
-                <BackButton/>
-            </View>
         </View>
     )
 }
 
 export const styles = StyleSheet.create({
     container: {
-        top: 100,
-        padding: 30,
+        marginTop: 40,
+        padding: 50,
         justifyContent: 'center',
         gap: 16,
         alignItems: 'center',
     },
     container2: {
-        top: 10,
+        top: 0,
     },
     head: {
         top: 20,
@@ -83,11 +93,16 @@ export const styles = StyleSheet.create({
         color: '#41423a',
     },
     image: {
-        width: 200,
-        height: 200,
+        maxHeight: 'auto',
+        maxWidth: 'auto',
+    },
+    image7: {
+        maxHeight: 'auto',
+        maxWidth: 'auto'
     },
     container3: {
-        alignItems: 'center'
+        alignItems: 'center',
+        maxHeight: 'auto'
     }
 
 })
